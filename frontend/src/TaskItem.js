d21157d627a629e2884543471b1a6ef56ff0ee10
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-07-31 01:16:23 -0400

    Connect frontend to backend API

--- frontend/src/TaskItem.js
+++ frontend/src/TaskItem.js
@@ -1,16 +1,28 @@
-export default function TaskItem({taskItem}) {
+export default function TaskItem({taskItem, onEdit, onDelete}) {
   return (
     <span className="has-hover-show">
       <label className="checkbox">
-        <input type="checkbox" />
-        &nbsp;{taskItem.description}
+        <input checked={taskItem.isDone}
+               onChange={() => onEdit({...taskItem, isDone: !taskItem.isDone})}
+               type="checkbox" />
+        <span className="ml-1">{taskItem.description}</span>
       </label>
-      <button className="button is-white is-small ml-1 mb-2 is-invisible hover-show" title="Edit description">
+      <button className="button is-white is-small ml-1 mb-2 is-invisible hover-show"
+              onClick={() => {
+                const newDescription = window.prompt('New Description:', taskItem.description);
+                onEdit({...taskItem, description: newDescription});
+              }}
+              title="Edit description">
         <span className="icon">
           <i className="fa-solid fa-pen-to-square"></i>
         </span>
       </button>
-      <button className="button is-warning is-light is-small ml-1 mb-2 is-invisible hover-show" title="Permanently delete task">
+      <button className="button is-warning is-light is-small ml-1 mb-2 is-invisible hover-show"
+              onClick={() => {
+                if (window.confirm('Are you sure you would like to permanently delete the following task: \n\n' + taskItem.description))
+                  onDelete(taskItem.id)
+              }}
+              title="Permanently delete task">
         <span className="icon">
           <i className="fa-solid fa-circle-minus"></i>
         </span>
--- frontend/src/TaskItemList.js
+++ frontend/src/TaskItemList.js
@@ -1,16 +1,128 @@
+import { useState, useEffect } from 'react';
 import TaskItem from "./TaskItem";
 
-export default function TaskItemList({items}) {
+// TODO: don't hard-code API domain here
+const rootApiUri = 'http://localhost:8000/api/tasks';
+
+export default function TaskItemList() {
+  const [taskItems, setTaskItems] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(rootApiUri);
+        if (!response.ok) {
+          throw new Error('Error retrieving tasks');
+        }
+        const allTaskItems = await response.json();
+        setTaskItems(allTaskItems);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleCreate = async (description) => {
+    try {
+      const response = await fetch(rootApiUri, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({description: description, isDone: false})
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete task");
+      }
+      else {
+        const newTaskItem = await response.json();
+        setTaskItems(currTaskItems => [...currTaskItems, newTaskItem]);
+      }
+    } catch (error) {
+      console.error("Create task failed:", error);
+      alert('Failed to create a task');
+    }
+  };
+
+  const handleEdit = async (updatedTaskItem) => {
+    try {
+      const response = await fetch(`${rootApiUri}/${updatedTaskItem.id}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(updatedTaskItem)
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to edit task");
+      }
+      else {
+        setTaskItems(currTaskItems => currTaskItems.map(x => {
+          if (x.id === updatedTaskItem.id) {
+            return {
+              ...x,
+              description: updatedTaskItem.description,
+              isDone: updatedTaskItem.isDone};
+          } else {
+            return x;
+          }
+        }));
+      }
+    } catch (error) {
+      console.error("Edit task failed:", error);
+      alert('Failed to edit task');
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`${rootApiUri}/${id}`, {
+        method: "DELETE"
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete task");
+      }
+      else {
+        setTaskItems(currTaskItems => currTaskItems.filter(x => x.id !== id));
+      }
+    } catch (error) {
+      console.error("Delete task failed:", error);
+      alert('Failed to delete task');
+    }
+  };
+
   return (
     <section className="block container">
-      <button className="button mb-3">Add Task</button>
-      <ul>
-        {items.map(x => (
-          <li key={x.id}>
-            <TaskItem taskItem={x} />
-          </li>
-        ))}
-      </ul>
+      <button className="button mb-3"
+              onClick={() => handleCreate(window.prompt('Task Description:'))}>
+        Add Task
+      </button>
+      {
+        loading ? (
+          <div>
+            <i className='fa-solid fa-spinner fa-spin mr-2' />
+            <span>Loading your tasks...</span>
+          </div>
+        ) : (
+            error ? (
+              <div className='notification is-warning is-light'>
+                {error}
+              </div>
+            ) : (
+                <ul>
+                  {taskItems.map(x => (
+                    <li key={x.id}>
+                      <TaskItem taskItem={x} onEdit={handleEdit} onDelete={handleDelete} />
+                    </li>
+                  ))}
+                </ul>
+              ))
+      }
     </section>
   );
 }
